f54db1d811fb19d74e4d3c989f07db4be08510a2
Stephen Lucero <stephen.lucero@mediacurrent.com>
2016-10-01 17:29:56 -0400

    Scaffold plugin manager and interface for import processors.

---
+++ src/ImportProcessorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\yaml_content\Plugin\YamlContent;
+
+
+use Drupal\Component\Plugin\PluginInspectionInterface;
+
+interface ImportProcessorInterface extends PluginInspectionInterface {
+
+}
---
+++ src/Plugin/YamlContent/ImportProcessorBase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\yaml_content\Plugin\YamlContent;
+
+
+use Drupal\Core\Plugin\PluginBase;
+
+class ImportProcessorBase extends PluginBase implements ImportProcessorInterface{
+
+}
