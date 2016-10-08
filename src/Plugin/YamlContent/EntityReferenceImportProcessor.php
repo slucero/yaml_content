45de6329b629fec131456de805d05b81c29175af
Stephen Lucero <stephen.lucero@mediacurrent.com>
2016-10-08 15:11:05 -0400

    Begin implementation of query building and execution logic.

--- src/Plugin/YamlContent/EntityReferenceImportProcessor.php
+++ src/Plugin/YamlContent/EntityReferenceImportProcessor.php
@@ -3,14 +3,17 @@
 namespace Drupal\yaml_content\Plugin\YamlContent;
 
 use Drupal\yaml_content\ImportProcessorBase;
+use Drupal\Core\Entity\Query\QueryInterface;
 
 /**
  * Import processor to support entity queries and references.
  *
+ * @todo Add support for returning multiple result matches.
+ *
  * @ImportProcessor(
  *   id = "entity_reference_import_processor",
  *   label = @Translation("Entity Reference Import Processor"),
- *   context = {
+ *   config = {
  *     "entity_type" = NULL,
  *     "filters" = NULL,
  *   },
@@ -19,14 +22,75 @@ use Drupal\yaml_content\ImportProcessorBase;
 class EntityReferenceImportProcessor extends ImportProcessorBase {
 
   /**
+   * @var \Drupal\Core\Entity\Query\QueryInterface
+   */
+  protected $query;
+
+  /**
    * {@inheritdoc}
    */
   public function execute() {
+    if (!$this->query) {
+      $this->buildQuery();
+    }
 
+    $results = $this->query->execute();
+
+    if (empty($results)) {
+      $this->noResults();
+    }
   }
 
+  /**
+   * Build and prepare the the entity query with configured filters.
+   *
+   * All filters are applied using the addQueryFilter() method. After executing
+   * this method, the entity query object itself is available from the $query
+   * property.
+   *
+   * @return \Drupal\Core\Entity\Query\QueryInterface
+   */
   protected function buildQuery() {
+    $entity_type = $this->configuration['entity_type'];
+    $this->query = \Drupal::entityQuery($entity_type);
 
+    // Apply filters.
+    foreach ($this->configuration['filters'] as $property => $value) {
+      $this->addQueryFilter($entity_type, $property, $value);
+    }
+
+    return $this->query;
+  }
+
+  /**
+   * Apply property filter to the entity query.
+   *
+   * This method may be overridden to provide further processing or mapping of
+   * property values as needed for filters.
+   *
+   * @param $entity_type
+   * @param $property
+   * @param $value
+   */
+  protected function addQueryFilter($entity_type, $property, $value) {
+    $this->query->condition($property, $value);
+  }
+
+  /**
+   * Handle behavior when no matches were found.
+   *
+   * @throws \Drupal\yaml_content\Plugin\YamlContent\MissingDataException
+   */
+  protected function noResults() {
+    $entity_type = $this->configuration['entity_type'];
+    $filters = $this->configuration['filters'];
+
+    $error = 'Unable to find referenced content of type %type matching: @filters';
+
+    throw new MissingDataException(__CLASS__ . ': ' . $this->t($error, array(
+        '%type' => $entity_type,
+        '@filters' => print_r($filters, TRUE),
+      )));
   }
 
   /**
