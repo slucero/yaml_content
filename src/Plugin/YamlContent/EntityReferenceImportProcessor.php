36848dc769aeb80342e95521a35cfba9460ebd8c
Stephen Lucero <stephen.lucero@mediacurrent.com>
2016-10-08 20:16:17 -0400

    Implement result processing for entity reference plugin.

--- src/Plugin/YamlContent/EntityReferenceImportProcessor.php
+++ src/Plugin/YamlContent/EntityReferenceImportProcessor.php
@@ -4,18 +4,18 @@ namespace Drupal\yaml_content\Plugin\YamlContent;
 
 use Drupal\yaml_content\ImportProcessorBase;
 use Drupal\Core\Entity\Query\QueryInterface;
+use Drupal\Core\TypedData\Exception\MissingDataException;
 
 /**
  * Import processor to support entity queries and references.
- *
- * @todo Add support for returning multiple result matches.
- *
+ **
  * @ImportProcessor(
  *   id = "entity_reference_import_processor",
  *   label = @Translation("Entity Reference Import Processor"),
  *   config = {
  *     "entity_type" = NULL,
  *     "filters" = NULL,
+ *     "limit" = 0,
  *   },
  * )
  */
@@ -37,8 +37,10 @@ class EntityReferenceImportProcessor extends ImportProcessorBase {
     $results = $this->query->execute();
 
     if (empty($results)) {
-      $this->noResults();
+      return $this->noResults();
     }
+
+    return $this->processResults($results);
   }
 
   /**
@@ -57,6 +59,11 @@ class EntityReferenceImportProcessor extends ImportProcessorBase {
     // Apply filters.
     foreach ($this->configuration['filters'] as $property => $value) {
       $this->addQueryFilter($entity_type, $property, $value);
+    }
+
+    // Handle any limits on the number of results allowed.
+    if (isset($this->configuration['limit']) && $this->configuration['limit'] > 0) {
+      $this->query->range(0, $this->configuration['limit']);
     }
 
     return $this->query;
@@ -91,6 +98,25 @@ class EntityReferenceImportProcessor extends ImportProcessorBase {
         '%type' => $entity_type,
         '@filters' => print_r($filters, TRUE),
       )));
+  }
+
+  /**
+   * Process entity ID results into expected content structure.
+   *
+   * @param array $result_set
+   * @return array
+   *   The content array for use as part of the import process.
+   */
+  protected function processResults(array $result_set) {
+    $field_data = array();
+
+    foreach ($result_set as $entity_id) {
+      $field_data[] = array(
+        'target_id' => $entity_id,
+      );
+    }
+
+    return $field_data;
   }
 
   /**
