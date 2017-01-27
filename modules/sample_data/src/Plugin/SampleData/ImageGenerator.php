4e4eddb2b9d7f42568aff00e3ea48ec7d43f6c04
Stephen Lucero <stephen.lucero@mediacurrent.com>
2017-01-27 11:33:47 -0500

    Implement initial image generator plugin.

---
+++ modules/sample_data/src/Plugin/SampleData/ImageGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\sample_data\Plugin\SampleData;
+
+use Drupal\Core\Annotation\Translation;
+use Drupal\sample_data\Annotation\SampleDataGenerator;
+use Drupal\sample_data\SampleDataGeneratorBase;
+
+/**
+ * Sample data generator to provide basic placeholder images.
+ *
+ * @SampleDataGenerator(
+ *   id = "image_generator",
+ *   title = @Translation("Sample image generator"),
+ *   description= = @Translation("A basic generator for placeholder images.")
+ * )
+ */
+class ImageGenerator extends SampleDataGeneratorBase {
+
+  /**
+   * Helper function to fetch images.
+   *
+   * @param string $width
+   *   The width of the image to fetch.
+   * @param string $height
+   *   The height of the image to fetch.
+   * @param string $ext
+   *   (Optional) The extension to use for fetching the image.
+   *
+   * @return \Drupal\file\FileInterface|false
+   *   The file entity that was created.
+   */
+  public function getImage($width, $height, $ext = "png") {
+    $image = file_get_contents("http://placehold.it/$width" . 'x' . $height);
+    return file_save_data(
+      $image,
+      "public://$width" . 'x' . "$height.$ext", FILE_EXISTS_REPLACE);
+  }
+}
---
+++ modules/sample_data/src/SampleDataGeneratorBase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\sample_data;
+
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Plugin\PluginBase;
+use Drupal\sample_data\Annotation\SampleDataGenerator;
+
+/**
+ * Base implementation for SampleDataGenerator plugins to extend.
+ */
+class SampleDataGeneratorBase extends PluginBase implements SampleDataGeneratorInterface {
+
+
+}
